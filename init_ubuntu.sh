0c5159e24a706cd9dac250d55b7ccc88d2a5b841
Jared Shaw <312950810@qq.com>
2021-03-16 22:49:41 +0800

    Usable now

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -20,24 +20,24 @@ printf "[$0] \"/etc/apt/sources.list\" is now backed up as \"/etc/apt/sources.li
 
 ## 用新源列表（清华源）覆盖旧源列表
 printf "[$0] New source list from Tsinghua University is writing into \"/etc/apt/sources.list\".\n"
-echo $pwd | sudo -S echo "# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释" > /etc/apt/sources.list
-echo $pwd | sudo -S echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse" >> /etc/apt/sources.list
-echo $pwd | sudo -S echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse" >> /etc/apt/sources.list
-echo $pwd | sudo -S echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse" >> /etc/apt/sources.list
-echo $pwd | sudo -S echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse" >> /etc/apt/sources.list
-echo $pwd | sudo -S echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse" >> /etc/apt/sources.list
-echo $pwd | sudo -S echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse" >> /etc/apt/sources.list
-echo $pwd | sudo -S echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse" >> /etc/apt/sources.list
-echo $pwd | sudo -S echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse" >> /etc/apt/sources.list
-echo $pwd | sudo -S echo ""
-echo $pwd | sudo -S echo "# 预发布软件源，不建议启用" >> /etc/apt/sources.list
-echo $pwd | sudo -S echo "# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse" >> /etc/apt/sources.list
-echo $pwd | sudo -S echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse" >> /etc/apt/sources.list
+echo "# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释" | sudo tee /etc/apt/sources.list
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
+echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
+echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
+echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
+echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
+echo ""
+echo "# 预发布软件源，不建议启用" | sudo tee -a /etc/apt/sources.list
+echo "# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
+echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
 printf "[$0] Sources list has been changed.\n"
 
 # 换源后的更新操作
 printf "[$0] Update & Upgrade using the new sources list.\n"
-echo $pwd | sudo -S apt update && sudo apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 
 # 提示脚本运行完毕
 printf "[$0] Initialization has finished.\n"
